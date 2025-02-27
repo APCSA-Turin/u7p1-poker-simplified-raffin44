e545f681746266c9caf54669a484d54733696361
raffin44 <nashwannafi@gmail.com>
2025-02-27 17:02:51 +0000

    finished

--- src/main/java/com/example/project/Deck.java
+++ src/main/java/com/example/project/Deck.java
@@ -16,15 +16,21 @@ public class Deck{
     }
 
     public  void initializeDeck(){ //hint.. use the utility class
-        
+        for(int i = 0; i < 4; i++){
+            for (int j = 0; j < 13; j++){
+                Card newCard = Card(Utility.getRanks()[i], Utility.getSuits()[j]);
+                cards.add(newCard);
+            }
+        }
     }
 
     public  void shuffleDeck(){ //You can use the Collections library or another method. You do not have to create your own shuffle algorithm
-        
+        Collections.shuffle(cards);
     }
 
     public  Card drawCard(){
-       return new Card("","");
+       Cards drawn = cards.get(Math.random()*53);
+        return drawn;
     }
 
     public  boolean isEmpty(){
